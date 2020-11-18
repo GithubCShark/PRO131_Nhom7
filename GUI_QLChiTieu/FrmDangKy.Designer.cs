3ae614b3d807ace2fea665f4202310229da40b38
ngocd <ngocdam2k@gmail.com>
2020-11-18 23:20:04 +0700

    Main,Login,Register

---
+++ GUI_QLChiTieu/FrmDangKy.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace GUI_QLChiTieu
+{
+    partial class FrmDangKy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDangKy = new System.Windows.Forms.Label();
+            this.lblHoTen = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblMatKhau = new System.Windows.Forms.Label();
+            this.lblNhapLai = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.txtMKNhapLai = new System.Windows.Forms.TextBox();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDangKy
+            // 
+            this.lblDangKy.AutoSize = true;
+            this.lblDangKy.Font = new System.Drawing.Font("Times New Roman", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDangKy.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblDangKy.Location = new System.Drawing.Point(81, 18);
+            this.lblDangKy.Name = "lblDangKy";
+            this.lblDangKy.Size = new System.Drawing.Size(114, 31);
+            this.lblDangKy.TabIndex = 0;
+            this.lblDangKy.Text = "Đăng Ký";
+            // 
+            // lblHoTen
+            // 
+            this.lblHoTen.AutoSize = true;
+            this.lblHoTen.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoTen.Location = new System.Drawing.Point(12, 68);
+            this.lblHoTen.Name = "lblHoTen";
+            this.lblHoTen.Size = new System.Drawing.Size(59, 19);
+            this.lblHoTen.TabIndex = 1;
+            this.lblHoTen.Text = "Họ Tên";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(12, 132);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(47, 19);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblMatKhau
+            // 
+            this.lblMatKhau.AutoSize = true;
+            this.lblMatKhau.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatKhau.Location = new System.Drawing.Point(12, 203);
+            this.lblMatKhau.Name = "lblMatKhau";
+            this.lblMatKhau.Size = new System.Drawing.Size(118, 19);
+            this.lblMatKhau.TabIndex = 3;
+            this.lblMatKhau.Text = "Nhập Mật Khẩu";
+            // 
+            // lblNhapLai
+            // 
+            this.lblNhapLai.AutoSize = true;
+            this.lblNhapLai.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNhapLai.Location = new System.Drawing.Point(12, 269);
+            this.lblNhapLai.Name = "lblNhapLai";
+            this.lblNhapLai.Size = new System.Drawing.Size(144, 19);
+            this.lblNhapLai.TabIndex = 4;
+            this.lblNhapLai.Text = "Nhập Lại Mật Khẩu";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(12, 89);
+            this.txtHoTen.Multiline = true;
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(260, 30);
+            this.txtHoTen.TabIndex = 5;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(12, 153);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(260, 30);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(12, 224);
+            this.txtMatKhau.Multiline = true;
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(260, 30);
+            this.txtMatKhau.TabIndex = 7;
+            // 
+            // txtMKNhapLai
+            // 
+            this.txtMKNhapLai.Location = new System.Drawing.Point(12, 291);
+            this.txtMKNhapLai.Multiline = true;
+            this.txtMKNhapLai.Name = "txtMKNhapLai";
+            this.txtMKNhapLai.Size = new System.Drawing.Size(260, 30);
+            this.txtMKNhapLai.TabIndex = 8;
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangKy.Location = new System.Drawing.Point(12, 359);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(120, 40);
+            this.btnDangKy.TabIndex = 9;
+            this.btnDangKy.Text = "Đăng Ký";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.Location = new System.Drawing.Point(152, 359);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(120, 40);
+            this.btnHuy.TabIndex = 10;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            // 
+            // FrmDangKy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 411);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.txtMKNhapLai);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.lblNhapLai);
+            this.Controls.Add(this.lblMatKhau);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblHoTen);
+            this.Controls.Add(this.lblDangKy);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmDangKy";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ĐĂNG KÝ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDangKy;
+        private System.Windows.Forms.Label lblHoTen;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblMatKhau;
+        private System.Windows.Forms.Label lblNhapLai;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.TextBox txtMKNhapLai;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.Button btnHuy;
+    }
+}
